c0b9b70229b8d2eecfc7f00e941e00dc4605c5f3
Shivam kumar <99358693+shivvammRec@users.noreply.github.com>
2022-09-24 17:09:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ export default function App() {
       .get("https://reqres.in/api/users/")
       .then((res) => {
         arr.push(...res.data.data);
-        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -26,7 +25,6 @@ export default function App() {
       .then((res) => {
         arr.push(...res.data.data);
         setData(arr);
-        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
